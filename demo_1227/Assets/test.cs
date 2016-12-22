fd1888d20c4e94fb6164e4dee095cdeb62fca3d0
MSI\Dyron <dyron85511@gmail.com>
2016-12-23 02:19:59 +0800

    12/23 02:19

---
+++ demo_1227/Assets/test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+    Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.V)) {
+            animator.SetBool("run", true);
+        }
+        if (Input.GetKeyUp(KeyCode.B))
+        {
+            animator.SetBool("run", false);
+        }
+    }
+}
